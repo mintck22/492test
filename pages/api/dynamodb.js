7ab9c18e5be7b065e75836a190cb801e85f0c236
Erzengel792 <tanadol1230@gmail.com>
2023-10-04 09:56:22 +0700

    fix api

--- pages/api/dynamodb.js
+++ pages/api/dynamodb.js
@@ -1,31 +1,27 @@
 import AWS from "aws-sdk";
 
-// Initialize the AWS DynamoDB client
-AWS.config.update({
-  region: "ap-southeast-1", // Replace with your desired AWS region
-  accessKeyId: "AKIA5MHKPM36GRLA7SYF", // Replace with your AWS access key
-  secretAccessKey: "tzgx7Z/0EzRSarnPGIfBKCoEFCdedPYcMHqDSNA9", // Replace with your AWS secret key
-});
+const dynamoDB = new AWS.DynamoDB({ region: "ap-southeast-1" });
+const documentClient = new AWS.DynamoDB.DocumentClient({ service: dynamoDB });
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
-
-export default async (req, res) => {
+export const handler = async (event) => {
   try {
     // Define the DynamoDB parameters for the query
     const params = {
-      TableName: "camera1", // Replace with your DynamoDB table name
-      ScanIndexForward: false, // Sort in descending order
-      Limit: 1, // Limit the result to 1 item (the newest)
-      KeyConditionExpression: "attribute_exists(UploadTime)", // Ensure UploadTime exists
-      ProjectionExpression: "ImageName, HumanCount, UploadTime", // Specify the attributes you want
+      TableName: "camera1",
+      ScanIndexForward: false,
+      Limit: 1,
+      KeyConditionExpression: "attribute_exists(UploadTime)",
+      ProjectionExpression: "HumanCount", // Only fetch HumanCount
     };
 
     // Use the query operation to retrieve the newest item from DynamoDB
-    const queryResult = await dynamoDB.query(params).promise();
+    const queryResult = await documentClient.query(params).promise();
 
     if (queryResult.Items.length === 0) {
-      // No items found
-      return res.status(404).json({ message: "No items found" });
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: "No items found" }),
+      };
     }
 
     const newestItem = queryResult.Items[0];
@@ -36,10 +32,24 @@ export default async (req, res) => {
     // Convert it to a number if needed (it's currently a string)
     const humanCountNumber = parseInt(humanCount, 10);
 
-    // Return the HumanCount as JSON
-    res.status(200).json({ HumanCount: humanCountNumber });
+    // Calculate the available seats
+    const totalSeats = 121; // Replace with your total number of seats
+    const availableSeats = totalSeats - humanCountNumber;
+
+    // Return only the HumanCount and availableSeats as JSON
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        HumanCount: humanCountNumber,
+        AvailableSeats: availableSeats,
+      }),
+    };
   } catch (error) {
     console.error("Error:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
   }
 };
